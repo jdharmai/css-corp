7f68041141f144f1173671a5a85f8c445f4b0c58
Jaganprabu <Jaganprabu.Dharmaiah@csscorp.com>
2021-12-17 12:44:04 +0530

    function component committed

--- ReactJS Project/src/index.js
+++ ReactJS Project/src/index.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import { Fragment } from "react";
 
-const App = ({title, caption}) => {
-    return (
-        <Fragment>
-            <h1>{title}</h1>
-            <h2 style={{
-                backgroundColor: true? "red": "blue",
-                color:"#fff"
-            }}>{caption}</h2>
-        </Fragment>    
-    )    
-};
+// const App = ({title, caption}) => {
+//     return (
+//         <Fragment>
+//             <h1>{title}</h1>
+//             <h2 style={{
+//                 backgroundColor: true? "red": "blue",
+//                 color:"#fff"
+//             }}>{caption}</h2>
+//         </Fragment>    
+//     )    
+// };
+
+class App extends Component{
+    render() {
+        const {title, caption} = this.props;
+        return (
+            <>
+                <h1>{title}</h1>
+                <h2>{caption}</h2>
+            </>
+        )
+    }
+}
 
 ReactDOM.render(
     <>

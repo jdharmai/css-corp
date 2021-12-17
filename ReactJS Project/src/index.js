00dea3fdf0a948056fa35d5e3fe4ac3759e317e8
Jaganprabu <Jaganprabu.Dharmaiah@csscorp.com>
2021-12-17 12:18:05 +0530

    function component committed

--- ReactJS Project/src/index.js
+++ ReactJS Project/src/index.js
@@ -2,14 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Fragment } from "react";
 
-const App = () => {
-    return <h1>Hello from functional component</h1>
+const App = ({title, caption}) => {
+    return (
+        <Fragment>
+            <h1>{title}</h1>
+            <h2 style={{
+                backgroundColor: true? "red": "blue",
+                color:"#fff"
+            }}>{caption}</h2>
+        </Fragment>    
+    )    
 };
 
 ReactDOM.render(
-    <Fragment>
+    <>
         <h1>Hello from ReactJS Tutorial</h1>
-        <App/>
-    </Fragment>,
+        <input type="text"/>
+        <App title="Good morning" caption="Have a nice day"/>
+    </>,
     document.getElementById("root"),
 )
